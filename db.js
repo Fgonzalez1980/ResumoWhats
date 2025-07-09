cb3400134134ba5bcf6b3a8e08b9a6b0c100185c
Fgonzalez1980 <flavio.gonzalez@terra.com.br>
2025-07-09 13:32:31 -0600

    Create db.js

---
+++ db.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool();
+
+const salvarMensagem = async (msg) => {
+  const query = `
+    INSERT INTO whatsapp_news (id, grupo, mensagem, autor, timestamp)
+    VALUES ($1, $2, $3, $4, $5)
+    ON CONFLICT (id) DO NOTHING
+  `;
+  const values = [msg.id, msg.grupo, msg.mensagem, msg.autor, msg.timestamp];
+  await pool.query(query, values);
+};
+
+module.exports = { salvarMensagem };
